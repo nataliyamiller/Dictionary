5b657179ffb41823cc211b82e54b9736eb77bcb9
Nataliya Miller <nataliyamiller82@gmail.com>
2015-08-21 09:27:32 -0700

    Definition class returns all instances of definition objects, test passes

--- src/test/java/DefinitionTest.java
+++ src/test/java/DefinitionTest.java
@@ -18,5 +18,15 @@ public class DefinitionTest {
     assertEquals("a unit of language, consisting of one or more spoken sounds", testDefinition.getDescription());
   }
 
+  @Test
+  public void all_returnsAllInstancesOfDefinition_true() {
+    Definition firstDefinition = new Definition("a unit of language, consisting of one or more spoken sounds.");
+    Definition secondDefinition = new Definition("a grammatical unit of one or more words that expresses an independent statement.");
+    assertTrue(Definition.all().contains(firstDefinition));
+    assertTrue(Definition.all().contains(secondDefinition));
+  }
+
+
+
 
 }
